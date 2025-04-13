cd3d6728ac84954e40f549ff48ce07e273d4bb2b
DESKTOP-VUB1IRJ\Thinkbook <nguyenhuuphong53@gmail.com>
2025-04-14 02:25:38 +0700

    fonts oled 1.3

--- Game_Trex/Core/Inc/fonts.h
+++ Game_Trex/Core/Inc/fonts.h
@@ -1,129 +1,59 @@
-/**
- * original author:  Tilen Majerle<tilen@majerle.eu>
- * modification for STM32f10x: Alexander Lutsai<s.lyra@ya.ru>
-   ----------------------------------------------------------------------
-   	Copyright (C) Alexander Lutsai, 2016
-    Copyright (C) Tilen Majerle, 2015
-    
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    any later version.
-     
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-   ----------------------------------------------------------------------
- */
 #ifndef FONTS_H
-#define FONTS_H 120
+#define FONTS_H
 
-/* C++ detection */
+#include "stm32f1xx_hal.h"
+#include <stdint.h>
+#include <stddef.h>
+
 #ifdef __cplusplus
-extern C {
+extern "C" {
 #endif
 
 /**
- *
- * Default fonts library. It is used in all LCD based libraries.
- *
- * \par Supported fonts
- * 
- * Currently, these fonts are supported:
- *  - 7 x 10 pixels
- *  - 11 x 18 pixels
- *  - 16 x 26 pixels
- */
-#include "stm32f1xx_hal.h"
-#include "string.h"
-
-/**
- * @defgroup LIB_Typedefs
- * @brief    Library Typedefs
- * @{
- */
-
-/**
- * @brief  Font structure used on my LCD libraries
+ * @brief Font definition structure.
  */
 typedef struct {
-	uint8_t FontWidth;    /*!< Font width in pixels */
-	uint8_t FontHeight;   /*!< Font height in pixels */
-	const uint16_t *data; /*!< Pointer to data font data array */
+    uint8_t width;   /**< Font width in pixels. */
+    uint8_t height;  /**< Font height in pixels. */
+    const uint16_t* data; /**< Pointer to font data array. */
 } FontDef_t;
 
-/** 
- * @brief  String length and height 
+/**
+ * @brief String size (length and height) in pixels.
  */
 typedef struct {
-	uint16_t Length;      /*!< String length in units of pixels */
-	uint16_t Height;      /*!< String height in units of pixels */
-} FONTS_SIZE_t;
+    uint16_t length; /**< String length in pixels. */
+    uint16_t height; /**< String height in pixels. */
+} FontSize_t;
 
 /**
- * @}
- */
-
-/**
- * @defgroup FONTS_FontVariables
- * @brief    Library font variables
- * @{
- */
-
-/**
- * @brief  7 x 10 pixels font size structure 
+ * @brief 7x10 pixel font.
  */
 extern FontDef_t Font_7x10;
 
 /**
- * @brief  11 x 18 pixels font size structure 
+ * @brief 11x18 pixel font.
  */
 extern FontDef_t Font_11x18;
 
 /**
- * @brief  16 x 26 pixels font size structure 
+ * @brief 16x26 pixel font.
  */
 extern FontDef_t Font_16x26;
 
 /**
- * @}
+ * @brief Calculates string size in pixels for a given font.
+ *
+ * @param str       String to calculate size for.
+ * @param size      Pointer to FontSize_t structure to store the result.
+ * @param font      Pointer to FontDef_t font to use.
+ *
+ * @return Pointer to the input string.
  */
- 
-/**
- * @defgroup FONTS_Functions
- * @brief    Library functions
- * @{
- */
+char* FONTS_GetStringSize(char* str, FontSize_t* size, FontDef_t* font);
 
-/**
- * @brief  Calculates string length and height in units of pixels depending on string and font used
- * @param  *str: String to be checked for length and height
- * @param  *SizeStruct: Pointer to empty @ref FONTS_SIZE_t structure where informations will be saved
- * @param  *Font: Pointer to @ref FontDef_t font used for calculations
- * @retval Pointer to string used for length and height
- */
-char* FONTS_GetStringSize(char* str, FONTS_SIZE_t* SizeStruct, FontDef_t* Font);
-
-/**
- * @}
- */
- 
-/**
- * @}
- */
- 
-/**
- * @}
- */
-
-/* C++ detection */
 #ifdef __cplusplus
 }
 #endif
 
- 
-#endif
+#endif // FONTS_H
